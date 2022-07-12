8f3dfab0ddaac7216ac38ed1c31c0f67cd117b2b
JerryGuooo <gzw1213@126.com>
2022-07-12 15:52:09 +0800

    test maven

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -1,16 +1,9 @@
-package com.bolingcavalry.mavendockerplugindemo.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
-
 @RestController
 public class Hello {
 
     @RequestMapping("/")
-    public String SayHello BABABABA(){
-    	//修改返回的字符串的内容
-        return "123456. Hello jenkins, " + new Date();
+    public String sayHello(){
+	    //修改返回的字符串的内容
+        return "abcdefg. Hello jenkins, " + new Date();
     }
 }
